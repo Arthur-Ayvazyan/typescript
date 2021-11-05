01b33c2409de8d9f5a573d83a5439408ac815bcb
Arthur Ayvazyan <arthur.ayvaz@gmail.com>
2021-11-05 16:04:12 +0400

    added simple decorator

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,19 @@
-class Department {
-    // private readonly _id: string;
-    // private name: string;
-
-    private hobbies: string[] = [];
-
-    constructor( private readonly _id: string, public name: string) {
-        // this.name = n;
-    }
-    addHobby(...hobby: string[]) {
-        this.hobbies.push(...hobby)
-    }
-    describe() {
-        console.log(`Department(${this._id}):  ${this.name}`);
-        console.log(`hobbies:  ${this.hobbies}`);
+function Logger(logString: string) {
+    return function(constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
     }
 }
 
-const accounting = new Department('1', 'Accounting');
-accounting.addHobby('eat', 'drink', 'dance');
+@Logger('LOGGING - PERSON')
+class Person{
+    name = 'Arthur';
 
-accounting.describe();
-// console.log(a)
+    constructor() {
+        console.log("Creating person object...");
+    }
+}
 
-console.log(accounting)
+const pers = new Person();
+
+// console.log(pers)
